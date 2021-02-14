53743414740e1e9163a002f1da4cdce17e0e772c
Finn-He <74175945+Finn-He@users.noreply.github.com>
2021-02-15 04:55:10 +0800

    Create connect.py

---
+++ connect.py
@@ -0,0 +1,7 @@
+# Student name: Chiyu He (Finn) ;   Student ID: 1147535 
+
+dbuser = "chiyuhe"
+dbpass = "Liu91Guang01" #"PUT YOUR PASSWORD HERE"
+dbhost = "chiyuhe-comp636.cwzwqfvyur9y.us-east-1.rds.amazonaws.com" #"PUT YOUR AWS Connect String here"
+dbport = "5432"
+dbname = "activitynight"
